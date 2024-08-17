73a40a843cbef15497ef1ed0026653d950cb7fc2
Takeshi Nakatani <ggtakec@gmail.com>
2024-08-17 06:13:23 +0000

    Refactored parallel get object request

--- src/curl.h
+++ src/curl.h
@@ -246,7 +246,6 @@ class S3fsCurl
         static std::unique_ptr<S3fsCurl> UploadMultipartPostRetryCallback(S3fsCurl* s3fscurl);
         static std::unique_ptr<S3fsCurl> CopyMultipartPostRetryCallback(S3fsCurl* s3fscurl);
         static std::unique_ptr<S3fsCurl> MixMultipartPostRetryCallback(S3fsCurl* s3fscurl);
-        static std::unique_ptr<S3fsCurl> ParallelGetObjectRetryCallback(S3fsCurl* s3fscurl);
 
         // lazy functions for set curl options
         static bool CopyMultipartPostSetCurlOpts(S3fsCurl* s3fscurl);
@@ -293,7 +292,6 @@ class S3fsCurl
         static std::unique_ptr<S3fsCurl> CreateParallelS3fsCurl(const char* tpath, int fd, off_t start, off_t size, int part_num, bool is_copy, etagpair* petag, const std::string& upload_id, int& result);
         static int ParallelMultipartUploadRequest(const char* tpath, const headers_t& meta, int fd);
         static int ParallelMixMultipartUploadRequest(const char* tpath, headers_t& meta, int fd, const fdpage_list_t& mixuppages);
-        static int ParallelGetObjectRequest(const char* tpath, int fd, off_t start, off_t size);
 
         // lazy functions for set curl options(public)
         static bool UploadMultipartPostSetCurlOpts(S3fsCurl* s3fscurl);

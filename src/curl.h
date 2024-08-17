b2e06ff556004a73ec8ba5ebaea01f4725cc6f0f
Takeshi Nakatani <ggtakec@gmail.com>
2024-08-17 06:13:23 +0000

    Refactored parallel get object request

--- src/curl.h
+++ src/curl.h
@@ -293,7 +293,6 @@ class S3fsCurl
         static std::unique_ptr<S3fsCurl> CreateParallelS3fsCurl(const char* tpath, int fd, off_t start, off_t size, int part_num, bool is_copy, etagpair* petag, const std::string& upload_id, int& result);
         static int ParallelMultipartUploadRequest(const char* tpath, const headers_t& meta, int fd);
         static int ParallelMixMultipartUploadRequest(const char* tpath, headers_t& meta, int fd, const fdpage_list_t& mixuppages);
-        static int ParallelGetObjectRequest(const char* tpath, int fd, off_t start, off_t size);
 
         // lazy functions for set curl options(public)
         static bool MultipartUploadPartSetCurlOpts(S3fsCurl* s3fscurl);
